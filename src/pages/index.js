fec66b35ab44028f4af7f49743e140f0c982131e
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-01 21:16:58 -0300

    wip: 1,2,3 testing...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1 style={{ color: `red`, fontSize: 72 }}>Hello Dev!</h1>
+      <h4>Seja bem vindo</h4>
+      <img src="https://source.unsplash.com/random/400x200" alt="Random Img" />
+    </>
+  )
 }
